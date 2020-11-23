8b4d8456158429257bd6eba65bb0057ab3da7b63
Shawn Conerton <conerton52@gmail.com>
2020-11-23 11:18:44 -0600

    Book 4 Chapter 1 completed. Working code.

---
+++ src/components/employees/Employees.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employees.css"
+
+export const Employees = () => (
+    <section className="employee">
+        <h3 className="employee__name">Susan Strong</h3>
+        <div className="employee__location">Humane South</div>
+    </section>
+)
